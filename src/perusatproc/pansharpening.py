ee5515d4f4bf74c28045b2d9cbbb88ebca706523
alan <alan@debian>
2020-01-22 15:46:02 -0300

    compress pansharpen result

--- src/perusatproc/pansharpening.py
+++ src/perusatproc/pansharpening.py
@@ -2,7 +2,7 @@ from perusatproc.util import run_command
 
 
 def pansharpen(inp, inxs, out):
-    base_cmd = "otbcli_BundleToPerfectSensor -inp {inp} -inxs {inxs} -out {out}"
+    base_cmd = 'otbcli_BundleToPerfectSensor -inp {inp} -inxs {inxs} -out "{out}?&gdal:co:COMPRESS=DEFLATE&gdal:co:TILED=YES" uint16'
 
     cmd = base_cmd.format(inp=inp, inxs=inxs, out=out)
 
