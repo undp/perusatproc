bf7a10ad0aa09034501ffaf28b03b9b264013323
Damián Silvani <damian@dymaxionlabs.com>
2020-05-29 15:33:21 +0000

    Remove compression and tiled on pansharpening result

--- src/perusatproc/pansharpening.py
+++ src/perusatproc/pansharpening.py
@@ -4,5 +4,5 @@ from perusatproc.util import run_command
 def pansharpen(inp, inxs, out):
     base_cmd = 'otbcli_BundleToPerfectSensor -inp {inp} ' \
         '-inxs {inxs} ' \
-        '-out "{out}?&gdal:co:COMPRESS=DEFLATE&gdal:co:TILED=YES" uint16'
+        '-out "{out}" uint16'
     run_command(base_cmd.format(inp=inp, inxs=inxs, out=out))
