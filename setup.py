a6456d3c8327dd65fd025657675b6a2a2220f558
Damián Silvani <munshkr@gmail.com>
2021-09-04 15:49:04 -0300

    Update pyscaffold package

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
 """
     Setup file for perusatproc.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.0a1.
+    This file was generated with PyScaffold 4.0.1.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-import sys
-
-from pkg_resources import VersionConflict, require
 from setuptools import setup
 
-try:
-    require('setuptools>=38.3')
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
-
 if __name__ == "__main__":
-    setup(use_pyscaffold=True,
-          package_data={
-              "perusatproc": ["data/egm96.grd", "data/dem/*"],
-          })
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"},
+              package_data={
+                  "perusatproc": ["data/egm96.grd", "data/dem/*"],
+              })
+    except:  # noqa
+        print("\n\nAn error occurred while building the project, "
+              "please ensure you have the most updated version of setuptools, "
+              "setuptools_scm and wheel with:\n"
+              "   pip install -U setuptools setuptools_scm wheel\n\n")
+        raise
